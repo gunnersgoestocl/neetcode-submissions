84a46e2fcc457ae893f5d8db42e464a3ae393fe9
Yuri Takigawa <144506517+gunnersgoestocl@users.noreply.github.com>
2026-06-06 11:59:05 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maximum = prices[-1]
+        maximumGap = 0
+        for i in range(len(prices)-2, -1, -1):
+            maximumGap = max(maximumGap, maximum-prices[i])
+            maximum = max(maximum, prices[i])
+        return maximumGap
+
+
+        
