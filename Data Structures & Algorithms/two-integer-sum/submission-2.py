c122d04425eb613c08bc8fe9ecb63573404f48ab
Yuri Takigawa <144506517+gunnersgoestocl@users.noreply.github.com>
2026-06-07 21:21:22 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        idx_dict = {}
+        for i, n in enumerate(nums):
+            if idx_dict.get(n) == None:
+                idx_dict[n] = [i]
+            else:
+                idx_dict[n].append(i) 
+        for n in nums:
+            value_list = idx_dict.get(target-n, [])
+            if len(value_list) == 1 and idx_dict[n][0] != value_list[0]:
+                value1 = idx_dict[n][0]
+                value2 = value_list[0]
+                return [min(value1, value2), max(value1, value2)]
+            elif len(value_list) > 1:
+                return value_list
+        return []
+        
+        
