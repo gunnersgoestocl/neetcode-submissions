bf85e7e66f21cf45c79df21dfd3e631c731baf42
Yuri Takigawa <144506517+gunnersgoestocl@users.noreply.github.com>
2026-06-07 21:00:31 -0400

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # memory-efficient, performance-good
+        one, two = 1, 1  # one step before, two step before
+        for i in range(2, n + 1):
+            tmp = one
+            one = (
+                one + two
+            )  # next one step before is the sum of the last one step before and the last two step before
+            two = tmp  # next two step before is the last one step before
+        return one
